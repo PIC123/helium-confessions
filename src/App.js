878fedf42a0519ab21730a89fefb513c1282e322
Phil Cherner <pcherner@mit.edu>
2024-10-18 15:03:01 -0400

    Remove comments to rerun build

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import AudioRecorder from "./AudioRecorder"; // Import the component
+import AudioRecorder from "./AudioRecorder";
 
 function App() {
   return (
     <div className="App" style={{ textAlign: "center", marginTop: "50px" }}>
-      <AudioRecorder /> {/* Render the AudioRecorder component */}
+      <AudioRecorder />
     </div>
   );
 }
